48ffa0224217befa5d9fe7d4689f9d640811d268
Bill Lorensen <bill.lorensen@gmail.com>
2015-11-05 20:14:34 -0500

    SYNC: Files modified on wiki.

--- Cxx/GeometricObjects/ColoredLines.cxx
+++ Cxx/GeometricObjects/ColoredLines.cxx
@@ -1,8 +1,7 @@
-#include <vtkVersion.h>
 #include <vtkSmartPointer.h>
 #include <vtkCellArray.h>
 #include <vtkCellData.h>
-#include <vtkDoubleArray.h>
+#include <vtkUnsignedCharArray.h>
 #include <vtkPoints.h>
 #include <vtkLine.h>
 #include <vtkPolyData.h>
@@ -11,95 +10,97 @@
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
- 
+
 int main(int, char *[])
 {
-  // Create three points. Join (Origin and P0) with a red line and
-  // (Origin and P1) with a green line
-  double origin[3] = {0.0, 0.0, 0.0};
-  double p0[3] = {1.0, 0.0, 0.0};
-  double p1[3] = {0.0, 1.0, 0.0};
- 
-  // Create a vtkPoints object and store the points in it
+  // Create the polydata where we will store all the geometric data
+  vtkSmartPointer<vtkPolyData> linesPolyData =
+    vtkSmartPointer<vtkPolyData>::New();
+  
+  
+  // Create three points
+  double origin[3] = { 0.0, 0.0, 0.0 };
+  double p0[3] = { 1.0, 0.0, 0.0 };
+  double p1[3] = { 0.0, 1.0, 0.0 };
+  
+  // Create a vtkPoints container and store the points in it
   vtkSmartPointer<vtkPoints> pts =
     vtkSmartPointer<vtkPoints>::New();
   pts->InsertNextPoint(origin);
   pts->InsertNextPoint(p0);
   pts->InsertNextPoint(p1);
- 
-  // Setup two colors - one for each line
-  unsigned char red[3] = {255, 0, 0};
-  unsigned char green[3] = {0, 255, 0};
- 
-  // Setup the colors array
-  vtkSmartPointer<vtkUnsignedCharArray> colors =
-    vtkSmartPointer<vtkUnsignedCharArray>::New();
-  colors->SetNumberOfComponents(3);
-  colors->SetName("Colors");
- 
-  // Add the colors we created to the colors array
-  colors->InsertNextTupleValue(red);
-  colors->InsertNextTupleValue(green);
- 
+  
+  // Add the points to the polydata container
+  linesPolyData->SetPoints(pts);
+  
+  
   // Create the first line (between Origin and P0)
   vtkSmartPointer<vtkLine> line0 =
     vtkSmartPointer<vtkLine>::New();
-  line0->GetPointIds()->SetId(0,0); //the second 0 is the index of the Origin in the vtkPoints
-  line0->GetPointIds()->SetId(1,1); //the second 1 is the index of P0 in the vtkPoints
- 
+  line0->GetPointIds()->SetId(0, 0); // the second 0 is the index of the Origin in linesPolyData's points
+  line0->GetPointIds()->SetId(1, 1); // the second 1 is the index of P0 in linesPolyData's points
+  
   // Create the second line (between Origin and P1)
   vtkSmartPointer<vtkLine> line1 =
     vtkSmartPointer<vtkLine>::New();
-  line1->GetPointIds()->SetId(0,0); //the second 0 is the index of the Origin in the vtkPoints
-  line1->GetPointIds()->SetId(1,2); //2 is the index of P1 in the vtkPoints
- 
-  // Create a cell array to store the lines in and add the lines to it
+  line1->GetPointIds()->SetId(0, 0); // the second 0 is the index of the Origin in linesPolyData's points
+  line1->GetPointIds()->SetId(1, 2); // 2 is the index of P1 in linesPolyData's points
+  
+  // Create a vtkCellArray container and store the lines in it
   vtkSmartPointer<vtkCellArray> lines =
     vtkSmartPointer<vtkCellArray>::New();
   lines->InsertNextCell(line0);
   lines->InsertNextCell(line1);
- 
-  // Create a polydata to store everything in
-  vtkSmartPointer<vtkPolyData> linesPolyData =
-    vtkSmartPointer<vtkPolyData>::New();
- 
-  // Add the points to the dataset
-  linesPolyData->SetPoints(pts);
- 
-  // Add the lines to the dataset
+  
+  // Add the lines to the polydata container
   linesPolyData->SetLines(lines);
- 
-  // Color the lines - associate the first component (red) of the
-  // colors array with the first component of the cell array (line 0)
-  // and the second component (green) of the colors array with the
-  // second component of the cell array (line 1)
+  
+  
+  // Create two colors - one for each line
+  unsigned char red[3] = { 255, 0, 0 };
+  unsigned char green[3] = { 0, 255, 0 };
+  
+  // Create a vtkUnsignedCharArray container and store the colors in it
+  vtkSmartPointer<vtkUnsignedCharArray> colors =
+    vtkSmartPointer<vtkUnsignedCharArray>::New();
+  colors->SetNumberOfComponents(3);
+  colors->InsertNextTupleValue(red);
+  colors->InsertNextTupleValue(green);
+  
+  // Color the lines.
+  // SetScalars() automatically associates the values in the data array passed as parameter
+  // to the elements in the same indices of the cell data array on which it is called.
+  // This means the first component (red) of the colors array
+  // is matched with the first component of the cell array (line 0)
+  // and the second component (green) of the colors array
+  // is matched with the second component of the cell array (line 1)
   linesPolyData->GetCellData()->SetScalars(colors);
- 
-  // Visualize
+  
+  
+  // Setup the visualization pipeline
   vtkSmartPointer<vtkPolyDataMapper> mapper =
     vtkSmartPointer<vtkPolyDataMapper>::New();
-#if VTK_MAJOR_VERSION <= 5
-  mapper->SetInput(linesPolyData);
-#else
   mapper->SetInputData(linesPolyData);
-#endif
-
+  
   vtkSmartPointer<vtkActor> actor =
     vtkSmartPointer<vtkActor>::New();
   actor->SetMapper(mapper);
- 
+  
   vtkSmartPointer<vtkRenderer> renderer =
     vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renderWindow =
-    vtkSmartPointer<vtkRenderWindow>::New();
-  renderWindow->AddRenderer(renderer);
-  vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor = 
-      vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  renderWindowInteractor->SetRenderWindow(renderWindow);
   renderer->AddActor(actor);
-
-  renderWindow->Render();
-  renderWindowInteractor->Start();
- 
+  
+  vtkSmartPointer<vtkRenderWindow> window =
+    vtkSmartPointer<vtkRenderWindow>::New();
+  window->AddRenderer(renderer);
+  
+  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+    vtkSmartPointer<vtkRenderWindowInteractor>::New();
+  interactor->SetRenderWindow(window);
+  
+  // Visualize
+  window->Render();
+  interactor->Start();
+  
   return EXIT_SUCCESS;
 }
--- Cxx/GeometricObjects/GeometricObjectsDemo.cxx
+++ Cxx/GeometricObjects/GeometricObjectsDemo.cxx
@@ -1,15 +1,12 @@
 #include <vtkSmartPointer.h>
-#include <vtkCamera.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkTextMapper.h>
 #include <vtkActor.h>
 #include <vtkActor2D.h>
-#include <vtkProperty.h>
-#include <vtkTextProperty.h>
-#include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
 #include <vtkRenderWindowInteractor.h>
- 
+
 #include <vtkArrowSource.h>
 #include <vtkConeSource.h>
 #include <vtkCubeSource.h>
@@ -18,106 +15,101 @@
 #include <vtkLineSource.h>
 #include <vtkRegularPolygonSource.h>
 #include <vtkSphereSource.h>
- 
+
 #include <vector>
- 
+
 int main(int, char *[])
 {
+  // Create container to hold the 3D object generators (sources)
   std::vector<vtkSmartPointer<vtkPolyDataAlgorithm> > geometricObjectSources;
- 
-  geometricObjectSources.push_back(vtkSmartPointer<vtkArrowSource>::New());
-  geometricObjectSources.push_back(vtkSmartPointer<vtkConeSource>::New());
-  geometricObjectSources.push_back(vtkSmartPointer<vtkCubeSource>::New());
-  geometricObjectSources.push_back(vtkSmartPointer<vtkCylinderSource>::New());
-  geometricObjectSources.push_back(vtkSmartPointer<vtkDiskSource>::New());
-  geometricObjectSources.push_back(vtkSmartPointer<vtkLineSource>::New());
-  geometricObjectSources.push_back(vtkSmartPointer<vtkRegularPolygonSource>::New());
-  geometricObjectSources.push_back(vtkSmartPointer<vtkSphereSource>::New());
   
+  // Populate the container with the various object sources to be demonstrated
+  geometricObjectSources.push_back( vtkSmartPointer<vtkArrowSource>::New() );
+  geometricObjectSources.push_back( vtkSmartPointer<vtkConeSource>::New() );
+  geometricObjectSources.push_back( vtkSmartPointer<vtkCubeSource>::New() );
+  geometricObjectSources.push_back( vtkSmartPointer<vtkCylinderSource>::New() );
+  geometricObjectSources.push_back( vtkSmartPointer<vtkDiskSource>::New() );
+  geometricObjectSources.push_back( vtkSmartPointer<vtkLineSource>::New() );
+  geometricObjectSources.push_back( vtkSmartPointer<vtkRegularPolygonSource>::New() );
+  geometricObjectSources.push_back( vtkSmartPointer<vtkSphereSource>::New() );
+  
+  // Create containers for the remaining nodes of each pipeline
   std::vector<vtkSmartPointer<vtkRenderer> > renderers;
   std::vector<vtkSmartPointer<vtkPolyDataMapper> > mappers;
   std::vector<vtkSmartPointer<vtkActor> > actors;
   std::vector<vtkSmartPointer<vtkTextMapper> > textmappers;
   std::vector<vtkSmartPointer<vtkActor2D> > textactors;
-
-  // Create one text property for all
-  vtkSmartPointer<vtkTextProperty> textProperty =
-    vtkSmartPointer<vtkTextProperty>::New();
-  textProperty->SetFontSize(12);
-  textProperty->SetJustificationToCentered();
-
-  // Create a source, renderer, mapper, and actor
-  // for each object 
-  for(unsigned int i = 0; i < geometricObjectSources.size(); i++)
-    {
+  
+  // Create a mapper and actor for each object and the corresponding text label
+  for( unsigned int i = 0; i < geometricObjectSources.size(); i++ )
+  {
     geometricObjectSources[i]->Update();
- 
-    mappers.push_back(vtkSmartPointer<vtkPolyDataMapper>::New());
-    mappers[i]->SetInputConnection(geometricObjectSources[i]->GetOutputPort());  
- 
-    actors.push_back(vtkSmartPointer<vtkActor>::New());
-    actors[i]->SetMapper(mappers[i]);
- 
-    textmappers.push_back(vtkSmartPointer<vtkTextMapper>::New());
-    textmappers[i]->SetInput(geometricObjectSources[i]->GetClassName());
-    textmappers[i]->SetTextProperty(textProperty);
-
-    textactors.push_back(vtkSmartPointer<vtkActor2D>::New());
-    textactors[i]->SetMapper(textmappers[i]);
-    textactors[i]->SetPosition(150, 16);
-
-    renderers.push_back(vtkSmartPointer<vtkRenderer>::New());
-    }
- 
-  int gridDimensions = 3;
- 
-  // Need a renderer even if there is no actor
-  for(size_t i = geometricObjectSources.size();
-      i < static_cast<size_t>(gridDimensions * gridDimensions);
-      i++)
-    {
-    renderers.push_back(vtkSmartPointer<vtkRenderer>::New());
-    } 
-
+    
+    mappers.push_back( vtkSmartPointer<vtkPolyDataMapper>::New() );
+    mappers[i]->SetInputConnection( geometricObjectSources[i]->GetOutputPort() );
+    
+    actors.push_back( vtkSmartPointer<vtkActor>::New() );
+    actors[i]->SetMapper( mappers[i] );
+    
+    textmappers.push_back( vtkSmartPointer<vtkTextMapper>::New() );
+    textmappers[i]->SetInput( geometricObjectSources[i]->GetClassName() ); // set text label to the name of the object source
+    
+    textactors.push_back( vtkSmartPointer<vtkActor2D>::New() );
+    textactors[i]->SetMapper( textmappers[i] );
+    textactors[i]->SetPosition( 10, 10); // Note: the position of an Actor2D is specified in display coordinates
+  }
+  
+  // Define size of the grid that will hold the objects
+  int gridCols = 3;
+  int gridRows = 3;
+  // Define side length (in pixels) of each renderer square
+  int rendererSize = 200;
+  
   vtkSmartPointer<vtkRenderWindow> renderWindow =
     vtkSmartPointer<vtkRenderWindow>::New();
-  int rendererSize = 200;
-  renderWindow->SetSize(rendererSize*gridDimensions, rendererSize*gridDimensions);
- 
+  renderWindow->SetSize(rendererSize*gridCols, rendererSize*gridRows);
+  
   vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
- 
-  for(int row = 0; row < gridDimensions; row++)
+  
+  // Set up a grid of viewports for each renderer
+  for( double row = 0; row < gridRows; row++ )
+  {
+    for( double col = 0; col < gridCols; col++ )
     {
-    for(int col = 0; col < gridDimensions; col++)
+      double index = row*gridCols + col;
+      
+      // Create a renderer for this grid cell
+      vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
+      renderer->SetBackground(.1, .2, .3);
+      
+      // Set the renderer's viewport dimensions (xmin, ymin, xmax, ymax) within the render window.
+      // Note that for the Y values, we need to subtract the row index from gridRows
+      // because the viewport Y axis points upwards, but we want to draw the grid from top to down
+      double viewport[4] = {
+        ((col    )              * rendererSize) / (gridCols * rendererSize),
+        ((gridRows - (row + 1)) * rendererSize) / (gridRows * rendererSize),
+        ((col + 1)              * rendererSize) / (gridCols * rendererSize),
+        ((gridRows - (row    )) * rendererSize) / (gridRows * rendererSize) };
+      renderer->SetViewport(viewport);
+      
+      // Add the corresponding actor and label for this grid cell, if they exist
+      if( index < geometricObjectSources.size() )
       {
-      int index = row*gridDimensions + col;
-
-      // (xmin, ymin, xmax, ymax)
-      double viewport[4] = {static_cast<double>(col) * rendererSize / (gridDimensions * rendererSize), 
-			    static_cast<double>(gridDimensions - (row+1)) * rendererSize / (gridDimensions * rendererSize), 
-			    static_cast<double>(col+1)*rendererSize / (gridDimensions * rendererSize), 
-			    static_cast<double>(gridDimensions - row) * rendererSize / (gridDimensions * rendererSize)};
- 
-      renderWindow->AddRenderer(renderers[index]);
-      renderers[index]->SetViewport(viewport);
-      if(index > static_cast<int>(geometricObjectSources.size() - 1))
-	{
-	continue;
-	}
-
-      renderers[index]->AddActor(actors[index]);
-      renderers[index]->AddActor(textactors[index]);
-      renderers[index]->SetBackground(.4, .3, .2);
+        renderer->AddActor(actors[index]);
+        renderer->AddActor(textactors[index]);
       }
+      
+      renderWindow->AddRenderer(renderer);
     }
- 
-  vtkSmartPointer<vtkRenderWindowInteractor> interactor = 
+  }
+  
+  vtkSmartPointer<vtkRenderWindowInteractor> interactor =
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
   interactor->SetRenderWindow(renderWindow);
- 
+  
   renderWindow->Render();
   interactor->Start();
- 
+  
   return EXIT_SUCCESS;
 }
