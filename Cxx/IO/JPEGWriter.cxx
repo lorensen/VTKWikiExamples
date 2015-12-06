33d4fe2b3627747ac71e8a6a66d116c3295494e0
Bill Lorensen <bill.lorensen@gmail.com>
2015-12-05 20:13:19 -0500

    SYNC: Files modified on wiki.

--- Cxx/IO/JPEGWriter.cxx
+++ Cxx/IO/JPEGWriter.cxx
@@ -1,43 +1,29 @@
-#include <vtkImageData.h>
-#include <vtkJPEGWriter.h>
 #include <vtkSmartPointer.h>
 #include <vtkImageCanvasSource2D.h>
-#include <vtkImageCast.h>
+#include <vtkJPEGWriter.h>
 
-int main ( int argc, char *argv[] )
+int main(int vtkNotUsed(argc), char* vtkNotUsed(argv)[])
 {
-  std::string outputFilename;
-  if ( argc > 1 )
-    {
-    outputFilename = argv[1];
-    }
-  else
-    {
-    outputFilename = "output.jpg";
-    }
+  std::string outputFilename = "output.jpg";
 
-  int extent[6] = {0, 99, 0, 99, 0, 0};
+  // Create a 100x100 image to save into the jpeg file
+  int extent[6] = { 0, 99, 0, 99, 0, 0 };
   vtkSmartPointer<vtkImageCanvasSource2D> imageSource =
-      vtkSmartPointer<vtkImageCanvasSource2D>::New();
-  imageSource->SetExtent(extent);
-  imageSource->SetScalarTypeToUnsignedChar();
-  imageSource->SetNumberOfScalarComponents(3);
-  imageSource->SetDrawColor(127, 45, 255);
-  imageSource->FillBox(0, 99, 0, 99);
-  imageSource->SetDrawColor(255,255,255);
-  imageSource->FillBox(40, 70, 20, 50);
-  imageSource->Update();
-
-  vtkSmartPointer<vtkImageCast> castFilter =
-      vtkSmartPointer<vtkImageCast>::New();
-  castFilter->SetOutputScalarTypeToUnsignedChar ();
-  castFilter->SetInputConnection(imageSource->GetOutputPort());
-  castFilter->Update();
+    vtkSmartPointer<vtkImageCanvasSource2D>::New();
+  imageSource->SetExtent( extent );
+  imageSource->SetScalarTypeToUnsignedChar(); // vtkJPEGWriter only accepts unsigned char input
+  imageSource->SetNumberOfScalarComponents( 3 ); // 3 color channels: Red, Green and Blue
+  // Fill the whole image with a blue background
+  imageSource->SetDrawColor( 0, 127, 255 );
+  imageSource->FillBox( extent[0], extent[1], extent[2], extent[3] );
+  // Paint a 30x30 white square into the image
+  imageSource->SetDrawColor( 255, 255, 255 );
+  imageSource->FillBox( 40, 70, 20, 50 );
 
   vtkSmartPointer<vtkJPEGWriter> writer =
     vtkSmartPointer<vtkJPEGWriter>::New();
-  writer->SetFileName(outputFilename.c_str());
-  writer->SetInputConnection(castFilter->GetOutputPort());
+  writer->SetFileName( outputFilename.c_str() );
+  writer->SetInputConnection( imageSource->GetOutputPort() );
   writer->Write();
 
   return EXIT_SUCCESS;
