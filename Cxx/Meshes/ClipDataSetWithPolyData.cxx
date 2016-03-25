5ae856ec475f797f166528a341085cade1e24a18
Bill Lorensen <bill.lorensen@gmail.com>
2016-03-25 13:45:22 -0700

    SYNC: Files modified on wiki.

--- Cxx/Meshes/ClipDataSetWithPolyData.cxx
+++ Cxx/Meshes/ClipDataSetWithPolyData.cxx
@@ -1,14 +1,16 @@
 #include <vtkSmartPointer.h>
-#include <vtkConeSource.h>
+
+#include <vtkClipDataSet.h>
 #include <vtkImplicitPolyDataDistance.h>
+#include <vtkConeSource.h>
 #include <vtkPointData.h>
 #include <vtkUnstructuredGrid.h>
 #include <vtkFloatArray.h>
 #include <vtkRectilinearGrid.h>
-#include <vtkClipDataSet.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkProperty.h>
 #include <vtkActor.h>
+#include <vtkCamera.h>
 #include <vtkRectilinearGridGeometryFilter.h>
 #include <vtkDataSetMapper.h>
 #include <vtkRenderer.h>
@@ -24,6 +26,9 @@ int main (int, char *[])
   vtkSmartPointer<vtkConeSource> cone =
     vtkSmartPointer<vtkConeSource>::New();
   cone->SetResolution(50);
+  cone->SetDirection(0,0,-1);
+  cone->SetHeight(3.0);
+  cone->CappingOn();
   cone->Update();
  
   // Implicit function that will be used to slice the mesh
@@ -37,19 +42,19 @@ int main (int, char *[])
     vtkSmartPointer<vtkFloatArray>::New();
   for (unsigned int i = 0; i < dimension; ++i)
     {
-    xCoords->InsertNextValue(-1.0 + i * 2.0 / static_cast<float>(dimension));
+    xCoords->InsertNextValue(-1.0 + i * 2.0 / static_cast<float>(dimension-1));
     }
   vtkSmartPointer<vtkFloatArray> yCoords =
     vtkSmartPointer<vtkFloatArray>::New();
   for (unsigned int i = 0; i < dimension; ++i)
     {
-    yCoords->InsertNextValue(-1.0 + i * 2.0 / static_cast<float>(dimension));
+    yCoords->InsertNextValue(-1.0 + i * 2.0 / static_cast<float>(dimension-1));
     }
   vtkSmartPointer<vtkFloatArray> zCoords =
     vtkSmartPointer<vtkFloatArray>::New();
   for (unsigned int i = 0; i < dimension; ++i)
     {
-    zCoords->InsertNextValue(-1.0 + i * 2.0 / static_cast<float>(dimension));
+    zCoords->InsertNextValue(-1.0 + i * 2.0 / static_cast<float>(dimension-1));
     }
   // The coordinates are assigned to the rectilinear grid. Make sure that
   // the number of values in each of the XCoordinates, YCoordinates,
@@ -62,7 +67,7 @@ int main (int, char *[])
   rgrid->SetXCoordinates(xCoords);
   rgrid->SetYCoordinates(yCoords);
   rgrid->SetZCoordinates(zCoords);
- 
+
   // Create an array to hold distance information
   vtkSmartPointer<vtkFloatArray> signedDistances =
     vtkSmartPointer<vtkFloatArray>::New();
@@ -87,7 +92,7 @@ int main (int, char *[])
   clipper->SetInputData(rgrid);
   clipper->InsideOutOn();
   clipper->SetValue(0.0);
-  clipper->SetValue(.1);
+  clipper->GenerateClippedOutputOn();
   clipper->Update();
 
   // --- mappers, actors, render, etc. ---
@@ -122,39 +127,68 @@ int main (int, char *[])
   clipperMapper->SetInputConnection(clipper->GetOutputPort());
   clipperMapper->ScalarVisibilityOff();
 
+  vtkSmartPointer<vtkDataSetMapper> clipperOutsideMapper =
+    vtkSmartPointer<vtkDataSetMapper>::New();
+  clipperOutsideMapper->SetInputConnection(clipper->GetOutputPort(1));
+  clipperOutsideMapper->ScalarVisibilityOff();
+
   vtkSmartPointer<vtkActor> clipperActor =
     vtkSmartPointer<vtkActor>::New();
   clipperActor->SetMapper(clipperMapper);
-  clipperActor->GetProperty()->SetRepresentationToWireframe();
   clipperActor->GetProperty()->SetColor(0.89,0.81,0.34); // banana
+
+  vtkSmartPointer<vtkActor> clipperOutsideActor =
+    vtkSmartPointer<vtkActor>::New();
+  clipperOutsideActor->SetMapper(clipperOutsideMapper);
+  clipperOutsideActor->GetProperty()->SetColor(0.89,0.81,0.34); // banana
  
   // A renderer and render window
-  vtkSmartPointer<vtkRenderer> renderer =
+  // Create a renderer, render window, and interactor
+  double leftViewport[4] = {0.0, 0.0, 0.5, 1.0};
+  vtkSmartPointer<vtkRenderer> leftRenderer =
     vtkSmartPointer<vtkRenderer>::New();
-  renderer->SetBackground(.4, .5, .6);
+  leftRenderer->SetViewport(leftViewport);
+  leftRenderer->SetBackground(.4, .5, .6);
+
+  double rightViewport[4] = {0.5, 0.0, 1.0, 1.0};
+  vtkSmartPointer<vtkRenderer> rightRenderer =
+    vtkSmartPointer<vtkRenderer>::New();
+  rightRenderer->SetViewport(rightViewport);
+  rightRenderer->SetBackground(.5, .6, .6);
  
   // add the actors
-//  renderer->AddActor(coneActor);
-  renderer->AddActor(wireActor);
-  renderer->AddActor(clipperActor);
+  leftRenderer->AddActor(wireActor);
+  leftRenderer->AddActor(clipperActor);
+  rightRenderer->AddActor(clipperOutsideActor);
  
   vtkSmartPointer<vtkRenderWindow> renwin =
     vtkSmartPointer<vtkRenderWindow>::New();
-  renwin->AddRenderer(renderer);
+  renwin->SetSize(640, 480);
+  renwin->AddRenderer(leftRenderer);
+  renwin->AddRenderer(rightRenderer);
 
   // An interactor
   vtkSmartPointer<vtkRenderWindowInteractor> interactor =
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
   interactor->SetRenderWindow(renwin);
  
-  // Start
+  // Share the camera
+
+  leftRenderer->GetActiveCamera()->SetPosition(0, -1, 0);
+  leftRenderer->GetActiveCamera()->SetFocalPoint(0, 0, 0);
+  leftRenderer->GetActiveCamera()->SetViewUp(0, 0, 1);
+  leftRenderer->GetActiveCamera()->Azimuth(30);
+  leftRenderer->GetActiveCamera()->Elevation(30);
+  leftRenderer->ResetCamera();
+  rightRenderer->SetActiveCamera(leftRenderer->GetActiveCamera());
+
   renwin->Render();
   interactor->Start();
 
   // Generate a report
   vtkIdType numberOfCells = clipper->GetOutput()->GetNumberOfCells();
   std::cout << "------------------------" << std::endl;
-  std::cout << "The clipped dataset contains a " << std::endl
+  std::cout << "The clipped dataset(inside) contains a " << std::endl
             << clipper->GetOutput()->GetClassName() 
             <<  " that has " << numberOfCells << " cells" << std::endl;
   typedef std::map<int,int> CellContainer;
@@ -166,6 +200,27 @@ int main (int, char *[])
 
   CellContainer::const_iterator it = cellMap.begin();
   while (it != cellMap.end())
+    {
+    std::cout << "\tCell type " 
+              << vtkCellTypes::GetClassNameFromTypeId(it->first)
+              << " occurs " << it->second << " times." << std::endl;
+    ++it;
+    }
+
+  numberOfCells = clipper->GetClippedOutput()->GetNumberOfCells();
+  std::cout << "------------------------" << std::endl;
+  std::cout << "The clipped dataset(outside) contains a " << std::endl
+            << clipper->GetClippedOutput()->GetClassName() 
+            <<  " that has " << numberOfCells << " cells" << std::endl;
+  typedef std::map<int,int> OutsideCellContainer;
+  CellContainer outsideCellMap;
+  for (vtkIdType i = 0; i < numberOfCells; i++)
+    {
+    outsideCellMap[clipper->GetClippedOutput()->GetCellType(i)]++;
+    }
+
+  it = outsideCellMap.begin();
+  while (it != outsideCellMap.end())
     {
     std::cout << "\tCell type " 
               << vtkCellTypes::GetClassNameFromTypeId(it->first)
