fc497532aaa76bc645e55164ce81b6add6c4f768
Bill Lorensen <bill.lorensen@gmail.com>
2016-04-25 20:12:04 -0400

    SYNC: Files modified on wiki.

--- Cxx/Widgets/ContourWidget.cxx
+++ Cxx/Widgets/ContourWidget.cxx
@@ -1,95 +1,83 @@
 #include <vtkSmartPointer.h>
-#include <vtkProperty.h>
+// To setup the ContourWidget and its representation:
 #include <vtkContourWidget.h>
+#include <vtkProperty.h>
 #include <vtkOrientedGlyphContourRepresentation.h>
-#include <vtkRenderer.h>
-#include <vtkRenderWindow.h>
-#include <vtkRenderWindowInteractor.h>
-#include <vtkCommand.h>
-#include <vtkDebugLeaks.h>
-#include <vtkCamera.h>
-#include <vtkPlane.h>
+// To create the geometry:
 #include <vtkPolyData.h>
 #include <vtkCellArray.h>
 #include <vtkPoints.h>
 #include <vtkMath.h>
-#include <vtkWidgetEvent.h>
-#include <vtkWidgetEventTranslator.h>
+// Usual VTK pipeline elements:
+#include <vtkRenderer.h>
+#include <vtkRenderWindow.h>
+#include <vtkRenderWindowInteractor.h>
+#include <vtkInteractorStyleTrackballCamera.h>
 
 int main( int argc, char *argv[] )
 {
-  // Create the RenderWindow, Renderer and both Actors
-  //
-  vtkSmartPointer<vtkRenderer> renderer = vtkSmartPointer<vtkRenderer>::New();
-  vtkSmartPointer<vtkRenderWindow> renderWindow = vtkSmartPointer<vtkRenderWindow>::New();
-  renderWindow->AddRenderer(renderer);
+    // Create the contour widget
+    vtkSmartPointer<vtkContourWidget> contourWidget =
+        vtkSmartPointer<vtkContourWidget>::New();
 
-  vtkSmartPointer<vtkRenderWindowInteractor> interactor = 
-      vtkSmartPointer<vtkRenderWindowInteractor>::New();
-  interactor->SetRenderWindow(renderWindow);
+    // Override the default representation for the contour widget to customize its look
+    vtkSmartPointer<vtkOrientedGlyphContourRepresentation> contourRepresentation =
+        vtkSmartPointer<vtkOrientedGlyphContourRepresentation>::New();
+    contourRepresentation->GetLinesProperty()->SetColor(1, 0, 0); // Set color to red
+    contourWidget->SetRepresentation(contourRepresentation);
 
-  renderer->SetBackground(0.1, 0.2, 0.4);
-  renderWindow->SetSize(600, 600);
-
-  vtkSmartPointer<vtkOrientedGlyphContourRepresentation> contourRep = 
-      vtkSmartPointer<vtkOrientedGlyphContourRepresentation>::New();
-  contourRep->GetLinesProperty()->SetColor(1, 0, 0); //set color to red
-
-  vtkSmartPointer<vtkContourWidget> contourWidget = 
-      vtkSmartPointer<vtkContourWidget>::New();
-  contourWidget->SetInteractor(interactor);
-  contourWidget->SetRepresentation(contourRep);
-  contourWidget->On();
-
-  for (int i = 0; i < argc; i++)
+    // Generate a set of points arranged in a circle
+    int numPts = 10;
+    vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
+    for (int i = 0; i < numPts; i++)
     {
-    if (strcmp("-Shift", argv[i]) == 0)
-      {
-      contourWidget->GetEventTranslator()->RemoveTranslation( 
-                                        vtkCommand::LeftButtonPressEvent );
-      contourWidget->GetEventTranslator()->SetTranslation( 
-                                        vtkCommand::LeftButtonPressEvent,
-                                        vtkWidgetEvent::Translate );
-      }
-    else if (strcmp("-Scale", argv[i]) == 0)
-      {
-      contourWidget->GetEventTranslator()->RemoveTranslation( 
-                                        vtkCommand::LeftButtonPressEvent );
-      contourWidget->GetEventTranslator()->SetTranslation( 
-                                        vtkCommand::LeftButtonPressEvent,
-                                        vtkWidgetEvent::Scale );
-      }
+        // Create numPts points evenly spread around a circumference of radius 0.1
+        const double angle = 2.0*vtkMath::Pi()*i/numPts;
+        points->InsertPoint(static_cast<vtkIdType>(i), 0.1*cos(angle), 0.1*sin(angle), 0.0 );
     }
 
+    // Create a cell array to connect the points into meaningful geometry
+    vtkIdType* vertexIndices = new vtkIdType[numPts+1];
+    for (int i = 0; i < numPts; i++) { vertexIndices[i] = static_cast<vtkIdType>(i); }
+    // Set the last vertex to 0; this means the last line segment will join the 19th point (vertices[19])
+    // with the first one (vertices[0]), thus closing the circle.
+    vertexIndices[numPts] = 0;
+    vtkSmartPointer<vtkCellArray> lines = vtkSmartPointer<vtkCellArray>::New();
+    lines->InsertNextCell(numPts+1, vertexIndices);
 
-  vtkSmartPointer<vtkPolyData> pd = vtkSmartPointer<vtkPolyData>::New();
+    // Create polydata to hold the geometry just created, and populate it
+    vtkSmartPointer<vtkPolyData> polydata = vtkSmartPointer<vtkPolyData>::New();
+    polydata->SetPoints(points);
+    polydata->SetLines(lines);
 
-  vtkSmartPointer<vtkPoints> points = vtkSmartPointer<vtkPoints>::New();
-  vtkSmartPointer<vtkCellArray> lines = vtkSmartPointer<vtkCellArray>::New();
-  vtkIdType* lineIndices = new vtkIdType[21];
-  for (int i = 0; i< 20; i++)
-    {
-    const double angle = 2.0*vtkMath::Pi()*i/20.0;
-    points->InsertPoint(static_cast<vtkIdType>(i), 0.1*cos(angle),
-                        0.1*sin(angle), 0.0 );
-    lineIndices[i] = static_cast<vtkIdType>(i);
-    }
+    // Create the renderer to visualize the scene
+    vtkSmartPointer<vtkRenderer> renderer =
+        vtkSmartPointer<vtkRenderer>::New();
+    renderer->SetBackground(0.1, 0.2, 0.4); // Set a dark blue background (default is black)
 
-  lineIndices[20] = 0;
-  lines->InsertNextCell(21,lineIndices);
-  delete [] lineIndices;
-  pd->SetPoints(points);
-  pd->SetLines(lines);
-  
-  contourWidget->Initialize(pd);
-  contourWidget->Render();
-  renderer->ResetCamera();
-  renderWindow->Render();
-    
-  interactor->Initialize();
-  interactor->Start();
-  
-  contourWidget->Off();
-  
-  return EXIT_SUCCESS;
+    // Create the GUI window to hold the rendered scene
+    vtkSmartPointer<vtkRenderWindow> renderWindow =
+        vtkSmartPointer<vtkRenderWindow>::New();
+    renderWindow->AddRenderer(renderer);
+
+    // Create the events manager for the renderer window
+    vtkSmartPointer<vtkRenderWindowInteractor> interactor =
+        vtkSmartPointer<vtkRenderWindowInteractor>::New();
+    interactor->SetRenderWindow(renderWindow);
+
+    // Use the "trackball camera" interactor style, rather than the default "joystick camera"
+    vtkSmartPointer<vtkInteractorStyleTrackballCamera> style =
+      vtkSmartPointer<vtkInteractorStyleTrackballCamera>::New();
+    interactor->SetInteractorStyle(style);
+
+    // Set up the contour widget within the visualization pipeline just assembled
+    contourWidget->SetInteractor(interactor);
+    contourWidget->On(); // Turn on the interactor observer
+    contourWidget->Initialize(polydata);
+    renderer->ResetCamera(); // Reposition camera to fit the scene elements
+
+    // Start the interaction
+    interactor->Start();
+
+    return EXIT_SUCCESS;
 }
