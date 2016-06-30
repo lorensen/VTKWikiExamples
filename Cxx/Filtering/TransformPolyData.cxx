b8a5d4336527fa24b768a1bd3fb5004d2b5e79ac
Bill Lorensen <bill.lorensen@gmail.com>
2016-06-29 20:22:48 -0400

    SYNC: Files modified on wiki.

--- Cxx/Filtering/TransformPolyData.cxx
+++ Cxx/Filtering/TransformPolyData.cxx
@@ -1,20 +1,35 @@
 #include <vtkSmartPointer.h>
-#include <vtkTransformPolyDataFilter.h>
 #include <vtkSphereSource.h>
 #include <vtkPolyData.h>
 #include <vtkTransform.h>
+#include <vtkTransformPolyDataFilter.h>
 #include <vtkPolyDataMapper.h>
 #include <vtkActor.h>
+#include <vtkProperty.h>
 #include <vtkRenderWindow.h>
 #include <vtkRenderer.h>
 #include <vtkRenderWindowInteractor.h>
-#include <vtkProperty.h>
 
 int main(int, char *[])
 {
+  // Create the polydata geometry
+
   vtkSmartPointer<vtkSphereSource> sphereSource =
     vtkSmartPointer<vtkSphereSource>::New();
   sphereSource->Update();
+
+  // Set up the actor to display the untransformed polydata
+ 
+  vtkSmartPointer<vtkPolyDataMapper> originalMapper =
+    vtkSmartPointer<vtkPolyDataMapper>::New();
+  originalMapper->SetInputConnection(sphereSource->GetOutputPort());
+
+  vtkSmartPointer<vtkActor> originalActor =
+    vtkSmartPointer<vtkActor>::New();
+  originalActor->SetMapper(originalMapper);
+  originalActor->GetProperty()->SetColor(1,0,0);
+
+  // Set up the transform filter
 
   vtkSmartPointer<vtkTransform> translation =
     vtkSmartPointer<vtkTransform>::New();
@@ -26,17 +41,8 @@ int main(int, char *[])
   transformFilter->SetTransform(translation);
   transformFilter->Update();
 
-  // Create a mapper and actor
-  vtkSmartPointer<vtkPolyDataMapper> originalMapper =
-    vtkSmartPointer<vtkPolyDataMapper>::New();
-  originalMapper->SetInputConnection(sphereSource->GetOutputPort());
+  // Set up the actor to display the transformed polydata
 
-  vtkSmartPointer<vtkActor> originalActor =
-    vtkSmartPointer<vtkActor>::New();
-  originalActor->SetMapper(originalMapper);
-  originalActor->GetProperty()->SetColor(1,0,0);
-
-  // Create a mapper and actor
   vtkSmartPointer<vtkPolyDataMapper> transformedMapper =
     vtkSmartPointer<vtkPolyDataMapper>::New();
   transformedMapper->SetInputConnection(transformFilter->GetOutputPort());
@@ -46,23 +52,22 @@ int main(int, char *[])
   transformedActor->SetMapper(transformedMapper);
   transformedActor->GetProperty()->SetColor(0,1,0);
 
-  // Create a renderer, render window, and interactor
+  // Set up the rest of the visualization pipeline
+
   vtkSmartPointer<vtkRenderer> renderer =
     vtkSmartPointer<vtkRenderer>::New();
+  renderer->AddActor(originalActor);
+  renderer->AddActor(transformedActor);
+  renderer->SetBackground(.3, .6, .3); // Set renderer's background color to green
+
   vtkSmartPointer<vtkRenderWindow> renderWindow =
     vtkSmartPointer<vtkRenderWindow>::New();
   renderWindow->AddRenderer(renderer);
+
   vtkSmartPointer<vtkRenderWindowInteractor> renderWindowInteractor =
     vtkSmartPointer<vtkRenderWindowInteractor>::New();
   renderWindowInteractor->SetRenderWindow(renderWindow);
 
-  // Add the actor to the scene
-  renderer->AddActor(originalActor);
-  renderer->AddActor(transformedActor);
-  renderer->SetBackground(.3, .6, .3); // Background color green
-
-  // Render and interact
-  renderWindow->Render();
   renderWindowInteractor->Start();
   
   return EXIT_SUCCESS;
