6fd0c0f9635578fc6b7fdcfabc8f7debc7e0d9b5
Bill Lorensen <bill.lorensen@gmail.com>
2016-10-14 10:54:13 -0400

    SYNC: Files modified on wiki.

--- Cxx/Developers/vtkTestFilter.cxx
+++ Cxx/Developers/vtkTestFilter.cxx
@@ -1,43 +1,26 @@
-#include "vtkSmartPointer.h"
+#include "vtkTestFilter.h"
  
 #include "vtkObjectFactory.h"
 #include "vtkStreamingDemandDrivenPipeline.h"
 #include "vtkInformationVector.h"
 #include "vtkInformation.h"
 #include "vtkDataObject.h"
-#include "vtkCallbackCommand.h"
-
-#include "vtkTestFilter.h"
+#include "vtkSmartPointer.h"
  
 vtkCxxRevisionMacro(vtkTestFilter, "$Revision: 1.70 $");
 vtkStandardNewMacro(vtkTestFilter);
-
-vtkTestFilter::vtkTestFilter()
-{
-  vtkSmartPointer<vtkCallbackCommand> progressCallback = 
-      vtkSmartPointer<vtkCallbackCommand>::New();
-  progressCallback->SetCallback(this->ProgressFunction);
-  
-  this->AddObserver(vtkCommand::ProgressEvent, progressCallback);
-}
- 
-void vtkTestFilter::ProgressFunction(vtkObject* caller, long unsigned int eventId, void* clientData, void* callData)
-{
-  vtkTestFilter* testFilter = static_cast<vtkTestFilter*>(caller);
-  cout << "Progress: " << testFilter->GetProgress() << endl;
-}
 
 int vtkTestFilter::RequestData(vtkInformation *vtkNotUsed(request),
                                              vtkInformationVector **inputVector,
                                              vtkInformationVector *outputVector)
 {
  
-  // get the info objects
+  // Get the info objects
   vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
   vtkInformation *outInfo = outputVector->GetInformationObject(0);
  
  
-  // get the input and ouptut
+  // Get the input and ouptut
   vtkPolyData *input = vtkPolyData::SafeDownCast(
       inInfo->Get(vtkDataObject::DATA_OBJECT()));
  
@@ -53,4 +36,3 @@ int vtkTestFilter::RequestData(vtkInformation *vtkNotUsed(request),
  
   return 1;
 }
- 
--- Cxx/Developers/vtkTestFilter.h
+++ Cxx/Developers/vtkTestFilter.h
@@ -7,17 +7,15 @@ class vtkTestFilter : public vtkPolyDataAlgorithm
 {
 public:
   vtkTypeRevisionMacro(vtkTestFilter,vtkAlgorithm);
-  
+ 
   static vtkTestFilter *New();
  
 protected:
-  vtkTestFilter();
+  vtkTestFilter(){}
   ~vtkTestFilter(){}
  
   int RequestData(vtkInformation *, vtkInformationVector **, vtkInformationVector *);
  
-  static void ProgressFunction(vtkObject* caller, long unsigned int eventId, void* clientData, void* callData);
-  
 private:
   vtkTestFilter(const vtkTestFilter&);  // Not implemented.
   void operator=(const vtkTestFilter&);  // Not implemented.
