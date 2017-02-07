d8726ffea0d3c3623b50ecc1a7bbb775495dc933
Bill Lorensen <bill.lorensen@gmail.com>
2017-02-07 12:50:56 -0500

    SYNC: Files modified on wiki.

--- Cxx/Developers/ImageAlgorithmFilter.cxx
+++ Cxx/Developers/ImageAlgorithmFilter.cxx
@@ -1,47 +1,56 @@
+#include <vtkSmartPointer.h>
+
 #include "vtkImageAlgorithmFilter.h"
-
 #include "vtkImageData.h"
-#include "vtkObjectFactory.h"
-#include "vtkStreamingDemandDrivenPipeline.h"
-#include "vtkInformationVector.h"
-#include "vtkInformation.h"
-#include "vtkDataObject.h"
-#include "vtkSmartPointer.h"
 
-vtkStandardNewMacro(vtkImageAlgorithmFilter);
+void PrintImage(vtkImageData* image);
 
-int vtkImageAlgorithmFilter::RequestData(vtkInformation *vtkNotUsed(request),
-                                             vtkInformationVector **inputVector,
-                                             vtkInformationVector *outputVector)
+int main (int argc, char *argv[])
 {
-  // Get the info objects
-  vtkInformation *inInfo = inputVector[0]->GetInformationObject(0);
-  vtkInformation *outInfo = outputVector->GetInformationObject(0);
-  
-  // Get the input and ouptut
-  vtkImageData *input = vtkImageData::SafeDownCast(
-      inInfo->Get(vtkDataObject::DATA_OBJECT()));
-  
-  vtkImageData *output = vtkImageData::SafeDownCast(
-      outInfo->Get(vtkDataObject::DATA_OBJECT()));
-    
-  vtkSmartPointer<vtkImageData> image =
+  vtkSmartPointer<vtkImageData> input =
     vtkSmartPointer<vtkImageData>::New();
-  image->ShallowCopy(input);
+  // Setup the image
+  input->SetDimensions(2,2,1);
+  input->AllocateScalars(VTK_DOUBLE,1);
   
-  image->SetScalarComponentFromDouble(0,0,0,0, 5.0);
+  // Fill every entry of the image data with "2.0"
+  int* dims = input->GetDimensions();
   
-  output->ShallowCopy(image);
+  for (int y=0; y<dims[1]; y++)
+    {
+    for (int x=0; x<dims[0]; x++)
+      {
+      input->SetScalarComponentFromDouble(x,y,0,0,2.0);
+      }
+    }
+  
+  std::cout << "Input image: " << std::endl;
+  PrintImage(input);
+  
+  vtkSmartPointer<vtkImageAlgorithmFilter> filter =
+    vtkSmartPointer<vtkImageAlgorithmFilter>::New();
+  filter->SetInputData(input);
+  filter->Update();
+  
+  vtkImageData* output = filter->GetOutput();
+  
+  std::cout << "Output image: " << std::endl;
+  PrintImage(output);
+  
+  return 0;
+}
 
-  // Without these lines, the output will appear real but will not work as the input to any other filters
-  int extent[6];
-  input->GetExtent(extent);
-  output->SetExtent(extent);
-  outInfo->Set(vtkStreamingDemandDrivenPipeline::UPDATE_EXTENT(),
-               extent,
-               6);
-  outInfo->Set(vtkStreamingDemandDrivenPipeline::WHOLE_EXTENT(),
-               extent,
-               6);
-  return 1;
+void PrintImage(vtkImageData* image)
+{
+  int* dims = image->GetDimensions();
+  
+  for (int y=0; y<dims[1]; y++)
+    {
+    for (int x=0; x<dims[0]; x++)
+      {
+      double v = image->GetScalarComponentAsDouble(x,y,0,0);
+      std::cout << v << " ";
+      }
+    std::cout << std::endl;
+    }
 }
